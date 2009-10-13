60dcfb2d61c25034a91dea11ab3c6d7ef40cfb7c
Ben Sauls <SixTwelveOhTwo@gmail.com>
2009-10-13 09:56:34 -0700

    Removing module, leaving functions in main file.

--- filecomp.py
+++ filecomp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python env
-import os,sys,hashlib,fcmod
+import os,sys,hashlIb
 
 """
 /*********************************\
@@ -23,13 +23,57 @@ if len(sys.argv) != 3:
 
 (onedir, twodir) = sys.argv[1], sys.argv[2]
 
+def sumfile(fobj):
+    '''Returns an md5 hash for an object with read() method.'''
+    m = hashlib.md5()
+    while True:
+        d = fobj.read(8096)
+        if not d:
+            break
+        m.update(d)
+    return m.hexdigest()
 
+def md5sum(fname):
+    '''Returns md5 of a file, or stdin if fname is "-".'''
+    if fname == '-':
+        ret = sumfile(sys.stdin)
+    else:
+        try:
+            f = file(fname, 'rb')
+        except:
+            return 'Failed to open file'
+        ret = sumfile(f)
+        f.close()
+    return ret
 
-print "#######################################"
+def complists(dir1, dir2):
+    dira = []
+    dirb = []
+    ''' List files in target directories. '''
+    for file in os.listdir(dir1):
+        dira.append(file)
+    for file in os.listdir(dir2):
+        dirb.append(file)
+    ''' Compare files and display disparities. '''
+    indir1 = set(dira).difference(set(dirb))
+    indir2 = set(dirb).difference(set(dira))
+    return (indir1, indir2, set(dira).intersection(set(dirb)))
+
+def comfiles(files, onedir, twodir):
+    firstdir = {}
+    secdir = {}
+    for f in files:
+        of = onedir + "\\" + f
+        sf = twodir + "\\" + f
+        firstdir[f] = md5sum(of)
+        secdir[f] = md5sum(sf)
+    for x in firstdir:
+        if firstdir[x] != secdir[x]:
+            print "File %s does not match!" % X"#######################################"
 print "Comparing \n%s \n%s" % (onedir, twodir)
 print "#######################################"
 
-setinfo = fcmod.complists(onedir, twodir)
+setinfo = complists(onedir, twodir)
 if len(setinfo[0]) > 0:
     print "------------------------------\nItems in \"%s\" and not in \"%s\":" % (onedir, twodir)
     for z in setinfo[0]:
@@ -41,4 +85,4 @@ if len(setinfo[0]) > 0:
     print "------------------------------"
     
 if len(setinfo[2]) > 0:
-       fcmod.comfiles(setinfo[2], onedir, twodir)
+       comfiles(setinfo[2], onedir, twodir)
